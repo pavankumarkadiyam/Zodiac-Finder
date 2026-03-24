26986f1ef798ae081bbef01fc5b31bf89ab24b2a
Pavan Kumar Kadiyam <pavankumarkadiyam21@gmail.com>
2026-03-24 07:09:43 +0530

    Lightning Record View Form Added

--- force-app/main/default/lwc/lightningDataServiceComponent/lightningDataServiceComponent.js
+++ force-app/main/default/lwc/lightningDataServiceComponent/lightningDataServiceComponent.js
@@ -1,7 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
+import CASE_NUMBER_FIELD from '@salesforce/schema/Case.CaseNumber';
+import CONTACT_EMAIL_FIELD from '@salesforce/schema/Case.ContactEmail';
+import SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
+import STATUS_FIELD from '@salesforce/schema/Case.Status';
+import PRIORITY_FIELD from '@salesforce/schema/Case.Priority';
 
 export default class LightningDataServiceComponent extends LightningElement {
-    recordId = '500gK00000XVPbqQAH';
+    cardTiltle;
+    isLightningFormEnabled = false;
+    isLightningViewFormEnabled = false;
+    @api recordId;
     objectApiName = 'Case'
-    fields = ['CaseNumber','ContactEmail','Subject','Status','Priority'];
+    fields = [CASE_NUMBER_FIELD.fieldApiName,CONTACT_EMAIL_FIELD.fieldApiName,SUBJECT_FIELD.fieldApiName,STATUS_FIELD.fieldApiName,PRIORITY_FIELD.fieldApiName];
+    caseNumber = 'CaseNumber';
+
+    handleShowLightningForm(){
+        this.cardTiltle = 'Lightning Record Form';
+        this.isLightningFormEnabled = true;
+        this.isLightningViewFormEnabled = false;
+    }
+    handleShowLightningViewForm(){
+        this.cardTiltle = 'Lightning Record View Form';
+        this.isLightningFormEnabled = false;
+        this.isLightningViewFormEnabled = true;
+    }
 }
